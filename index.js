998c2b07737142035298f057969b9f8c37a5aa41
FunctionUP-Teju <kapilsharma68@gmail.com>
2023-05-08 14:09:24 +0530

    session/middleware-1 completed

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use('/',function(req,res,next){
+    const data = new Date()
+    
+    console.log('Time :'+' '+data.getHours()+':'+
+    data.getMinutes()+':'+
+    data.getSeconds())
+    console.log('Date :'+' '+data.getDate()+'/'+
+    data.getMonth()+'/'+
+    data.getFullYear())
+    
+    const ip = req.connection.remoteAddress
+    console.log(ip)
+    
+    next()
+})
+
 const port = 4000 
 app.listen(port,()=>{
     console.log(`Server is running at ${port}`)
