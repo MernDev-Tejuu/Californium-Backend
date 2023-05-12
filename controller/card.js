c2aa54d82e712ef6532eda9d59b0869a77768b45
FunctionUP-Teju <kapilsharma68@gmail.com>
2023-05-12 18:51:05 +0530

    JAY KISAN PROJECT COMPLETED

--- controller/card.js
+++ controller/card.js
@@ -2,26 +2,35 @@ const card = require('../../model/card')
 const customer = require('../../model/customer')
 const mongoose = require('mongoose')
 const create_Card = async(req,res)=>{
-//    
+  // Accessing Data from request body    
   try{  const body = req.body
+  //Stored Each properties in an arr   
     let arr = ['cardNumber','cardType','cardName','status','vision','customerId']
+  //Itterating over each property and checking whenther its present in body or not  
     for(i=0;i<arr.length;i++){
         if(!body.hasOwnProperty(arr[i]))
         return res.status(400).send({msg : `${arr[i]} is mandatory`})
     }
+   //Destructuring properties from body 
     const{cardNumber,customerId}=body
     const finder = await card.find({cardNumber})
     console.log(finder.length)
+//validation:1 Checking whether given cardNo already exist or not    
     if(!finder.length <= 0)
     return res.status(422).send({msg : `${cardNumber} already exist`})
+//Validation:2 Checking whether customerId(ObjectID/d) is valid     
     if(!mongoose.Types.ObjectId.isValid(customerId))
     return res.status(400).send({msg : `Customer-ID is invalid `})
+    //Checking ObjectId Using mongoose filter
     const finder2 = await customer.findById(customerId)
+//Validation:3 Checks whether the document belongs to this Id is deleted or not    
    if(finder2.isDeleted===true)
    return res.status(422).send({msg : 'This Customer was deleted'})
     const finder3 = await card.findById(customerId)
+//Validation:4 Checks whether card has already created using Object ID     
     if(!finder3 === null)
     return res.status(422).send({msg : `Customer already exist `})
+   //  All Validations Passed    
     const creator = await card.create(body)
     return res.status(201).send({Card_Created : creator})
 }
--- controller/customer.js
+++ controller/customer.js
@@ -1,34 +1,40 @@
 const customer = require('../../model/customer')
-const upperCase =require('upper-case')
 const mongoose = require('mongoose')
 const create_Customer = async(req,res)=>{
-    try{const body = req.body
+    try{
+//Accessing Data From request body..        
+    const body = req.body
+//Stored Property names in string form     
     let arr = ['firstName','emailId','lastName','mobileNumber','DOB','address','customerId']
+//Itterating each property strings  to check whether its present in the body or not   
     for(i=0;i<arr.length;i++){
         if(!body.hasOwnProperty(arr[i]))
         return res.status(422).send({msg : `${arr[i]} is mandatory`})
     }
+//Destructuring properties from = body    
     const{emailId,mobileNumber,customerId}=body
+//Validation:1 Checking Email is valid or not    
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(!emailId.match(emailRegex))
     return res.status(400).send({msg : 'Invalid email address. Please provide a valid email address in the format [example@example.com]."'})
     const finder = await customer.find({emailId})
-    console.log(finder.length )
+//validation:2 Checking whether email already exist before creation     
     if(finder.length > 0)
     return res.status(422).send({msg : `EmailId is already exist`}) 
+//Validation:3 Checking whether Mobile number is valid     
     if(mobileNumber.length <10 )
     return res.status(400).send({msg : 'Mobile is not valid '})
     const finder1 =  await customer.find({mobileNumber})
-    console.log(finder1)
+    //Checking Mobile is already present or not [Duplicate Key Error]    
     if(finder1.length > 0)
     return res.status(422).send({msg : `mobileNumber ${mobileNumber} is already exist`}) 
     const finder2 = await customer.find({customerId})
-
-    console.log("count" ,finder2.length)
+//Validation:4 Checks whether CustomerID has already present 
     if(finder2.length > 0)
     return res.status(422).send({msg : 'customerId already exist'})
-    // return res.send(`Created`)
+//  All Validations Passed/ 
     const creator = await customer.create(body) 
+//Counting number of documents being created
     const DocumentCount  = await customer.find().count()
     return res.status(201).send({Customer_Created : creator , totalDocuments : DocumentCount})    
 }
@@ -38,13 +44,19 @@ const create_Customer = async(req,res)=>{
 }
 
 const deleteCustomer = async(req,res)=>{
-    try{const query = req.query.ObjectId
-
+    try{
+//Accessing Data From request query..        
+    const query = req.query.ObjectId
+//Asking User For ObjectID    
     if(!query)return res.status(422).send({msg : `please provide ObjectId`})
+//validation:1 Testing whether given ObjectId is Valid or not    
     if(!mongoose.Types.ObjectId.isValid(query))
     return res.status(400).send({msg : `ObjectId is not valid`})
+
     const finder = await customer.findById(query)
-     finder.isDeleted=false
+  // Flag set to false
+    finder.isDeleted=false
+//Updating and making true    
     const finder1 = await customer.findOneAndUpdate({_id :query},{$set :{ isDeleted :true}},{new : true})
     return res.status(201).send({cust_Deleted : finder1})
     }
@@ -53,11 +65,16 @@ const deleteCustomer = async(req,res)=>{
     }
 }
 const get_Active_Customer=async(req,res)=>{
-   try{ const finder = await customer.find({status:'ACTIVE'})
+   try{ 
+//Filter will send all documents which has status:'ACTIVE'    
+    const finder = await customer.find({status:'ACTIVE'})
+//Filter will send all documents, so will remove the documents which was deleted    
     const mapper = finder.filter(x =>  x.isDeleted===true)
      finder.splice(finder.indexOf(mapper),1)
+//If No Active Customer Found     
     if(finder<=0)return res.status(200).send({msg :`No customer with Active status`})
     return res.status(200).send({all_Customer : finder})}
+// Still if something went wring ,the it will handle catch error    
    catch(err){
     return res.status(500).send(`Internal Error [${err.message}]`)
    }
