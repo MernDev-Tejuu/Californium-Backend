202e1854974851f5c74343a3dee3342c028d7305
MernDev-Tejuu <131139437+MernDev-Tejuu@users.noreply.github.com>
2023-05-19 10:25:43 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const product_Controller = require('./controller/product')
 const user_Middleware = require('../middleware/MW')
 const user_Controller  = require('./controller/user')
 const order_Controller = require('./controller/order')
+//Roll no 11 session/middleware2
 app.post('/product' , product_Controller.create_Product)
 app.post('/user',user_Middleware.validation,user_Controller.create_User)
 app.post('/order',user_Middleware.validation,order_Controller.create_Order)
